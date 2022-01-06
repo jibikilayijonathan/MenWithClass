b522262d6bc423d42ea05842f499f0cfa75b27b7
jonathan <jonathan.jibikilayi@esprit.tn>
2022-01-06 08:32:41 +0100

    formulaire d inscription

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,10 +15,18 @@ class RegisterController extends AbstractController
     /**
      * @Route("/inscription", name="register")
      */
-    public function index(): Response
+    public function index( Request $request)
     {
         $user = new User();
         $form= $this->createForm(RegisterType::class,$user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $user=$form->getData();
+            $doctrine=$this->getDoctrine()->getManager();
+            $doctrine->persist($user);
+            $doctrine->flush();
+            dd($user);
+        }
         return $this->render('register/index.html.twig',['form'=>$form->createView()]);
     }
 }
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -6,9 +6,13 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Validator\Type\SubmitTypeValidatorExtension;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use function Sodium\add;
 
 class RegisterType extends AbstractType
 {
@@ -17,20 +21,37 @@ class RegisterType extends AbstractType
         $builder
             ->add('firstname',TextType::class,['label'=> 'votre prenom',
                 'attr'=>[
-                    'placeholder'=>'MERCI POUR VOTRE PRENOM'
+                    'placeholder'=>'Merci pour votre prenom'
                 ]
             ])
             ->add('lastname',TextType::class,['label'=> 'votre nom','attr'=>[
-                'placeholder'=>'MERCI POUR VOTRE NOM'
+                'placeholder'=>'Veuillez entrer votre nom'
             ]])
-            ->add('email',EmailType::class,['label'=> 'votre EMAIL','attr'=>[
-                'placeholder'=>'MERCI POUR VOTRE EMAIL'
+            ->add('email',EmailType::class,['label'=> 'votre email','attr'=>[
+                'placeholder'=>'Merci pour votre Email'
             ]])
-            ->add('password',PasswordType::class,['label'=> 'votre mot de passe','attr'=>[
-                'placeholder'=>'MERCI POUR VOTRE mot de passe'
-            ]])
+            ->add('password',RepeatedType::class,[
+                'type'=> PasswordType::class,
+                'invalid_message'=>'le mot de passe et la confirmation doivent etre identique',
+                'label'=> 'votre mot de passe',
+                'required'=>true,
+                'fisrt_options'=>['label'=>'Mot de passe'],
+                'second_options'=>['label'=>'Mot de passe']
 
+            ])
+            ->add('password_confirm',PasswordType::class,
+                [
+                    'label'=> 'Confirmez votre mot de passe',
+                    'mapped'=> false,
+                    'attr'=>[
+                'placeholder'=>'MERCI POUR VOTRE mot de passe'
+
+            ]
+                ])
+            ->add('submit', SubmitType::class,['label'=> 's inscrire']);
         ;
+
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
